786deb53a3e9ccbe3f8433ea6d9d5bda7506d1f4
Andrey Demyanov <demyanov23@gmail.com>
2020-06-26 16:40:12 +0300

    Подготовил проект к защите.

--- js/popup.js
+++ js/popup.js
@@ -94,26 +94,3 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
-
-// Basket popup
-
-for (let basketLink of basketLinks){
-	basketLink.addEventListener("click", function (evt) {
-	  evt.preventDefault();
-	  basketPopup.classList.add("popup-show");
-	});
-}
-
-basketClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  basketPopup.classList.remove("popup-show");
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (basketPopup.classList.contains("popup-show")) {
-      evt.preventDefault();
-      basketPopup.classList.remove("popup-show");
-    }
-  }
-});
